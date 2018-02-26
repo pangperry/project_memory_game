440b06442bf8a3c12fd1adbb06c8ee17d7bd30d1
Mark Perry <mark.j.perry11@gmail.com>
2018-02-26 15:36:29 -0800

    Add shuffle and display cards

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log('hello initial setup!');
+$(function() {
+  var shuffleCards = function (list) {
+    var length = list.length;
+    var newLength = length;
+    var tempList = list.slice();
+    var shuffled = [];
+
+    for (var i = 0; i < length; i++) {
+      var index = Math.random() * newLength;
+      shuffled.push(tempList.splice(index, 1));
+    }
+    return shuffled;
+  };
+
+  var display = function(cards) {
+    $ul = $('ul');
+    for (var card = 0; card < cards.length; card++) {
+      $ul.append(cards[card]);
+    }
+  }
+
+  var cards = $('li');
+  var shuffled = shuffleCards(cards);
+  display(shuffled);
+  
+  // console.log(shuffled);
+});
