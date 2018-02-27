74a59402a1dab8529a09073305c117eaf74184af
Mark Perry <mark.j.perry11@gmail.com>
2018-02-26 23:34:45 -0800

    Add timer

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ $(function() {
       var index = Math.random() * newLength;
       shuffled.push(tempList.splice(index, 1));
     }
+
     return shuffled;
   };
 
@@ -17,11 +18,40 @@ $(function() {
     for (var card = 0; card < cards.length; card++) {
       $ul.append(cards[card]);
     }
-  }
+  };
+
+  var startTimer = function() {
+    var start = Date.now();
+
+    setInterval(function() {
+      var currentTime = Date.now();
+      var elapsed = currentTime - start;
+      var totalSeconds = Math.floor(elapsed / 1000);
+
+      var times = {
+        seconds : Math.floor(totalSeconds % 60), 
+        minutes : Math.floor(totalSeconds / 60 % 60),
+        hours : Math.floor(totalSeconds  / 1000 / 60 / 60 % 24),
+      };
+
+      (['hours', 'minutes', 'seconds']).forEach(unit => {
+        if (times[unit]) {
+          $(`#${unit}`).text(`${unit} ${times[unit]}`);
+        }
+      });
+
+    }, 1000);
+  };
+
+  $('.flip').click(function () {
+    $(this).find('.card').addClass('flipped').mouseleave(function () {
+      $(this).removeClass('flipped');
+    });
+    return true;
+  });
+
 
   var cards = $('li');
   var shuffled = shuffleCards(cards);
-  display(shuffled);
-  
-  // console.log(shuffled);
+  startTimer();
 });
